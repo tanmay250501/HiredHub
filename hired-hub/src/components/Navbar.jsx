7ad358cacd53e897046cfd09c4d125e7c99a1e45
tanmay250501 <tanmaysawwalakhe2@gmail.com>
2025-01-11 14:16:41 +0530

    adding authentication

--- hired-hub/src/components/Navbar.jsx
+++ hired-hub/src/components/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
-import { assets } from '/src/assets/assets.js';
+import { assets } from '../assets/assets.js';
+import  {useClerk, UserButton, useUser} from '@clerk/clerk-react';
 
 const Navbar = () => {
 
+    const {openSignIn} = useClerk();
+
     return(
-        <div>
-            <div>
-                <img src={assets.Logo} alt="logo" />
+        <div className="shadow py-3">
+            <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
+                <img src={assets.logo} alt="logo" />
+                <div className="flex gap-4 max-sm:text-xs">
+                    <button className="text-gray-600">Recruter Login</button>
+                    <button onClick={ e => openSignIn()} className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full">Login</button>
+                </div>
             </div>
         </div>
     )
--- hired-hub/src/main.jsx
+++ hired-hub/src/main.jsx
@@ -3,13 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { AppContextProvider } from './context/AppContext.jsx'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
 
+
+//Clerk Publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-
-  </BrowserRouter>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
+  </ClerkProvider>
 )

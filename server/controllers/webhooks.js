db8613e30618ba59632d726b0eda30b0494457e4
tanmay250501 <tanmaysawwalakhe2@gmail.com>
2025-02-10 14:03:40 +0530

    big issues fixed

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,67 +1,58 @@
-import {Webhook} from "svix"
-import User from "../models/User.js"
+import { Webhook } from "svix";
+import User from "../models/User.js";
 
-// API Controller to manage Clerk User with database
-
-export const clerkWebhooks = async (req,res) => {
-
+export const clerkWebhooks = async (req, res) => {
     try {
+        // Create a Svix instance with Clerk webhook secret
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-        // Create a Svix instance with clerk webhook secret.
-        const whook = new Webhook (process.env.CLERK_WEBHOOK_SECRET)
-
-        // Verifying Header 
-        await whook.verify(JSON.stringify(req.body),{
-            "svix-id": req.header["svix-id"],
-            "svix-timestamp": req.header["svix-timestamp"],
-            "svix-signature": req.header["svix-signature"]
-        })
+        // Verifying Headers
+        await whook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        });
 
         // Getting data from request body
-        const {data , type} = req.body
+        const { data, type } = req.body;
 
-        // Switch cases for diffrent 
         switch (type) {
-            case "user.created":{
-                
+            case "user.created": {
                 const userData = {
-                    _id : data.id,
-                    email: data.email_addresses[0].email_addresses,
-                    name: data.first_name + " " + data.last_name,
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,  // Fix here
+                    name: `${data.first_name} ${data.last_name}`,
                     image: data.image_url,
-                    resume:''
-                }
-                await User.create(userData)
-                res.json({})
-                break;
-            }
-            case "user.updated":{
-                const userData = {
-                    email: data.email_addresses[0].email_addresses,
-                    name: data.first_name + " " + data.last_name,
-                    image: data.image_url,
-                    resume:''
-                }
-                await User.findByIdAndUpdate(data.id, userData )
-                res.json({})
+                    resume: ''
+                };
+                await User.create(userData);
+                res.status(201).json({ success: true, message: "User created" });
                 break;
             }
 
-            case "user.deleted":{
-
-                await User.findByIdAndDelete(data.is)
-                res.json({})
-            }
-                
+            case "user.updated": {
+                const userData = {
+                    email: data.email_addresses[0].email_address,  // Fix here
+                    name: `${data.first_name} ${data.last_name}`,
+                    image: data.image_url,
+                    resume: ''
+                };
+                await User.findByIdAndUpdate(data.id, userData);
+                res.status(200).json({ success: true, message: "User updated" });
                 break;
-        
+            }
+
+            case "user.deleted": {
+                await User.findByIdAndDelete(data.id);
+                res.status(200).json({ success: true, message: "User deleted" });
+                break;
+            }
+
             default:
-                break;
+                res.status(400).json({ success: false, message: "Invalid webhook event" });
         }
-        
     } catch (error) {
-        console.log(error.message);
-        res.json({success:false,message:"Webhooks Error"})
-        
+        console.error(error);
+        res.status(500).json({ success: false, message: "Webhook Error" });
     }
-}
+};
--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,12 @@
-import mongoose from "mongoose"
-
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    _id: { type: String, required: "true" },
-    name: { type: String, required: "true" },
-    email: { type: String, required: "true", unique: "true" },
+    _id: { type: String, required: true }, 
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     resume: { type: String },
-    image: { type: String, required: "true" },
+    image: { type: String, required: true }
+});
 
-})
-
-const User = mongoose.model('User', userSchema)
-
+const User = mongoose.model('User', userSchema);
 export default User;
